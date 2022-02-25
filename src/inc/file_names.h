fcc7028227f4a3737e101026029e56628aaf74cf
ananthm1254 <ananth7121999@gmail.com>
2022-02-25 07:51:10 -0800

    Initial Commit

---
+++ src/inc/file_names.h
@@ -0,0 +1,37 @@
+/**************************************************************
+This is a auto-generated file. Please run the following command
+               python3 file_name_generator                     
+**************************************************************/
+
+#pragma once
+
+#define BLOCK_MAX_CNT        (4)
+#define PAGE_MAX_CNT         (4)
+
+static char gFileNames[BLOCK_MAX_CNT][PAGE_MAX_CNT][256] = 
+{
+	{
+		"/file_stack/file_block_0x0/file_unit_0x0.bin",
+		"/file_stack/file_block_0x0/file_unit_0x1.bin",
+		"/file_stack/file_block_0x0/file_unit_0x2.bin",
+		"/file_stack/file_block_0x0/file_unit_0x3.bin",
+	},
+	{
+		"/file_stack/file_block_0x1/file_unit_0x0.bin",
+		"/file_stack/file_block_0x1/file_unit_0x1.bin",
+		"/file_stack/file_block_0x1/file_unit_0x2.bin",
+		"/file_stack/file_block_0x1/file_unit_0x3.bin",
+	},
+	{
+		"/file_stack/file_block_0x2/file_unit_0x0.bin",
+		"/file_stack/file_block_0x2/file_unit_0x1.bin",
+		"/file_stack/file_block_0x2/file_unit_0x2.bin",
+		"/file_stack/file_block_0x2/file_unit_0x3.bin",
+	},
+	{
+		"/file_stack/file_block_0x3/file_unit_0x0.bin",
+		"/file_stack/file_block_0x3/file_unit_0x1.bin",
+		"/file_stack/file_block_0x3/file_unit_0x2.bin",
+		"/file_stack/file_block_0x3/file_unit_0x3.bin",
+	},
+};
